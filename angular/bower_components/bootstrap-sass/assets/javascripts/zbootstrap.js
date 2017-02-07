70a5f49122000dbef0f13d4fdf47443b42fa7bee
陈仲英 <chenzhongying@chenzhongyingdeiMac.local>
2017-02-07 18:56:23 +0800

    修改zbootstrap.js

--- angular/bower_components/bootstrap-sass/assets/javascripts/zbootstrap.js
+++ angular/bower_components/bootstrap-sass/assets/javascripts/zbootstrap.js
@@ -64,5 +64,130 @@ $.fn.extend({
 
         });
         return this;
+    },
+    upload: function (config) {
+        return this.each(function () {
+            $(this).on("change", function () {
+                var files = this.files,
+                    dataType = $(this).attr("data-type"),
+                    multiple = $(this).prop("multiple");
+                switch (dataType) {
+                    case "image":
+                        for (var i = 0; i < files.length; i++) {
+                            var name = files[i].name,
+                                fileSize = files[i].size;
+                            if (!config.fileTypeExts.test(name)) {
+                                alert(files[i].name + "文件格式不正确");
+                            } else if (fileSize / 1024 / 1024 > config.fileSize) {
+                                alert(files[i].name + "文件过大");
+                            } else {
+
+                                (function (i) {
+                                    var img = new Image();
+                                    img.src = window.URL.createObjectURL(files[i]);
+                                    console.log(img.src);
+                                    img.onload = function () {
+                                        if (config.width && (img.width != config.width)) {
+                                            alert(files[i].name + "图片宽度不正确");
+                                        } else if (config.height && (img.height != config.height)) {
+                                            alert(files[i].name + "图片高度不正确");
+                                        } else if ((config.maxHeight && (img.height > config.maxHeight)) || (config.maxWidth && (img.width > config.maxWidth))) {
+                                            alert(files[i].name + "图片尺寸不正确");
+                                        } else {
+                                            $xhr(files[i], i + 1, config);
+                                        }
+                                        window.URL.revokeObjectURL(img.src);
+                                    };
+                                })(i);
+                            }
+                        }
+                        break;
+                    case "doc":
+                        for (var i = 0; i < files.length; i++) {
+                            var name = files[i].name,
+                                fileSize = files[i].size;
+                            if (!config.fileTypeExts.test(name)) {
+                                alert(name + "文件格式不正确");
+                            } else if (fileSize / 1024 / 1024 > config.fileSize) {
+                                alert(name + "文件过大");
+                            } else {
+                                $xhr(files[i], config);
+                            }
+                        }
+                }
+
+            });
+        })
     }
 });
+$(".preview").on("click",".glyphicon-remove-sign",function(){
+    $(this).parents(".thumbnail-box").remove();
+});
+ // 上传文件添加dom树
+function thumbnailHtml(data) {
+    console.log(data);
+    var html = '<div class="col-xs-6 col-md-2 thumbnail-box">\
+                    <div class="thumbnail text-danger">\
+                    <span class="glyphicon glyphicon-remove-sign"></span>\
+                    <img src='+data.responseJSON.icon+' alt="...">\
+                    </div>\
+                </div>';
+    return html;
+}
+// 根据参数个数判断是上传图片还是文件
+function $xhr(arg1, arg2, arg3) {
+    var data = new FormData();
+    data.append("file", arg1);
+    switch (arguments.length) {
+        case 3:
+            $.ajax({
+                processData: false,
+                type: "POST",
+                contentType: false,
+                url: arg3.url,
+                data: data,
+                complete: function (data) {
+                    arg3.preview(data, arg2);
+                },
+                xhr: function () { //获取ajaxSettings中的xhr对象，为它的upload属性绑定progress事件的处理函数
+                    var myXhr = $.ajaxSettings.xhr();
+                    if (myXhr.upload) { //检查upload属性是否存在
+                        //绑定progress事件的回调函数
+                        myXhr.upload.addEventListener('progress', function (e) {
+                            if (e.lengthComputable) {
+                                var bili = e.loaded / e.total;
+                                console.log(bili);
+                            }
+                        }, false);
+                    }
+                    return myXhr; //xhr对象返回给jQuery使用
+                }
+            });
+            break;
+        case 2:
+            $.ajax({
+                processData: false,
+                type: "POST",
+                contentType: false,
+                url: arg2.url,
+                data: data,
+                complete: function (data) {
+                    arg2.preview(data);
+                },
+                xhr: function () { //获取ajaxSettings中的xhr对象，为它的upload属性绑定progress事件的处理函数
+                    var myXhr = $.ajaxSettings.xhr();
+                    if (myXhr.upload) { //检查upload属性是否存在
+                        //绑定progress事件的回调函数
+                        myXhr.upload.addEventListener('progress', function (e) {
+                            if (e.lengthComputable) {
+                                var bili = (e.loaded / e.total).toFixed(2);
+                                $("#" + arg2.progress + " .progress-bar").css("width", bili * 100 + "px").text(bili * 100 + "%");
+                            }
+                        }, false);
+                    }
+                    return myXhr; //xhr对象返回给jQuery使用
+                }
+            });
+            break;
+    }
+}
